2e61fadaca947da8c25a80d307a63cfe5d362905
Kavinay <kavi_kavinay@yahoo.com>
2026-09-10 13:40:23 +1200

    refactor: remove third-party service workers
    Clean up legacy service worker registrations and scripts to improve performance and prevent unauthorized third-party tracking.

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,12 @@
-self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 11480525
-}
-self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+// Self-unregister legacy service workers
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    self.registration.unregister().then(() => {
+      return self.clients.matchAll();
+    })
+  );
+});
